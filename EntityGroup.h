cc690630cfda57235c487bd707a2030140121db3
saphalpdyl <saphalpdyl@gmail.com>
2023-10-31 08:32:41 +0545

    Added friend operators

--- EntityGroup.h
+++ EntityGroup.h
@@ -8,12 +8,6 @@ class EntityGroup {
   EntityGroup(int numberCount, float damage, float health);
   EntityGroup();
 
-  // Overload Operators
-  friend std::ostream& operator<<(std::ostream& os, EntityGroup& obj) {
-    return os << "Numbers : " << obj.numberCount << "\nDamage : " << obj.damage
-              << "\nHealth : " << obj.health;
-  }
-
  private:
   int numberCount;
   float damage, health;
--- Game.cpp
+++ Game.cpp
@@ -3,8 +3,9 @@
 void Game::init() {
   ninjas = EntityGroup(10, 20.0f, 30.0f);
   ghosts = EntityGroup(20, 30.0f, 40.0f);
-
-  std::cout << ninjas << std::endl;
 }
 
-void Game::start() {}
+void Game::start() {
+  while (true) {
+  }
+}
